d09299c5271f29fc5047c8e30a640cbe7834402e
MinhUchiha <nqminh27894@gmail.com>
2017-12-05 09:14:19 +0700

    convertKanaToOneByte

--- UserEvent/SII_UE_Custpayment_Head.js
+++ UserEvent/SII_UE_Custpayment_Head.js
@@ -55,20 +55,32 @@ function(serverWidget,url,runtime,record, redirect, search) {
                         fieldId: 'custrecord_sii_custpayment_bank',
                         line: i
                     });
+                    bank = bank.replace(/\s/g, '');
+                    bank = convertKanaToOneByte(bank);
                     var branchname = currentRecord.getSublistValue({
                         sublistId: 'recmachcustrecord_sii_custpayment_h_id',
                         fieldId: 'custrecord_sii_custpayment_branchoff',
                         line: i
                     });
+                    branchname = branchname.replace(/\s/g, '');
+                    branchname = convertKanaToOneByte(branchname);
                     var request = currentRecord.getSublistValue({
                         sublistId: 'recmachcustrecord_sii_custpayment_h_id',
                         fieldId: 'custrecord_sii_custpayment_request',
                         line: i
                     });
+                    request = request.replace(/\s/g, '');
+                    request = convertKanaToOneByte(request);
                     for(var m =0; m < customerArray.length; m++){
-                        var custbankname = customerArray[m].getValue({name: 'custentity_sii_custpayment_bankname'}); 
+                        var custbankname = customerArray[m].getValue({name: 'custentity_sii_custpayment_bankname'});
+                        custbankname = custbankname.replace(/\s/g, '');
+                        custbankname = convertKanaToOneByte(custbankname);
                         var custbranchname = customerArray[m].getValue({name: 'custentity_sii_custpayment_branchname'});
+                        custbranchname = custbranchname.replace(/\s/g, '');
+                        custbranchname = convertKanaToOneByte(custbranchname);
                         var custclientname = customerArray[m].getValue({name: 'custentity_sii_custpayment_clientname'});
+                        custclientname = custclientname.replace(/\s/g, '');
+                        custclientname = convertKanaToOneByte(custclientname);
                         var entityid = customerArray[m].getValue({name: 'entityid'});
                         if(custbankname == bank && custbranchname == branchname && custclientname == request ){
                             currentRecord.setSublistValue({
@@ -95,6 +107,77 @@ function(serverWidget,url,runtime,record, redirect, search) {
         }
     }
 
+    // マップ作成用関数
+    function createKanaMap(properties, values) {
+        var kanaMap = {};
+        // 念のため文字数が同じかどうかをチェックする(ちゃんとマッピングできるか)
+        if(properties.length === values.length) {
+            for(var i=0, len=properties.length; i<len; i++) {
+                var property= properties.charCodeAt(i),
+                    value = values.charCodeAt(i);
+                kanaMap[property] = value;
+            }
+        }
+        return kanaMap;
+    };
+
+    // 全角から半角への変換用マップ
+    var m = createKanaMap(
+        'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンァィゥェォッャュョ',
+        'ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｧｨｩｪｫｯｬｭｮ'
+    );
+    // 半角から全角への変換用マップ
+    /*var mm = createKanaMap(
+        'ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｧｨｩｪｫｯｬｭｮ',
+        'アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲンァィゥェォッャュョ'
+    );*/
+
+    // 全角から半角への変換用マップ
+    var g = createKanaMap(
+        'ガギグゲゴザジズゼゾダヂヅデドバビブベボ',
+        'ｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾊﾋﾌﾍﾎ'
+    );
+    // 半角から全角への変換用マップ
+    /*var gg = createKanaMap(
+        'ｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾊﾋﾌﾍﾎ',
+        'ガギグゲゴザジズゼゾダヂヅデドバビブベボ'
+    );*/
+      
+    // 全角から半角への変換用マップ
+    var p = createKanaMap(
+        'パピプペポ',
+        'ﾊﾋﾌﾍﾎ'
+    );
+    // 半角から全角への変換用マップ
+    /*var pp = createKanaMap(
+        'ﾊﾋﾌﾍﾎ',
+        'パピプペポ'
+    );*/
+
+    var gMark = 'ﾞ'.charCodeAt(0);
+    var pMark = 'ﾟ'.charCodeAt(0);
+
+    function convertKanaToOneByte(str) {
+        for(var i=0, len=str.length; i<len; i++) {
+            if(g.hasOwnProperty(str.charCodeAt(i)) || p.hasOwnProperty(str.charCodeAt(i))) {
+                if(g[str.charCodeAt(i)]) {
+                    str = str.replace(str[i], String.fromCharCode(g[str.charCodeAt(i)])+String.fromCharCode(gMark));
+                }
+                else if(p[str.charCodeAt(i)]) {
+                    str = str.replace(str[i], String.fromCharCode(p[str.charCodeAt(i)])+String.fromCharCode(pMark));
+                }else {
+                    break;
+                }
+                i++;
+                len = str.length;
+            }else {
+                if(m[str.charCodeAt(i)]) {
+                    str = str.replace(str[i], String.fromCharCode(m[str.charCodeAt(i)]));
+                }
+            }
+        }
+        return str;
+    }
     /**
      * Function definition to be triggered before record is loaded.
      *
